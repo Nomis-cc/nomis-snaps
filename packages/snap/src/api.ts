4a93d84c4d30d71c8f8c255844e2778b8a80d535
Nikita Sukhachev <traed.traed@mail.ru>
2024-06-27 19:24:58 +0700

    feat: add home screen

---
+++ packages/snap/src/api.ts
@@ -0,0 +1,20 @@
+// const getData = async (url: string) => {
+//   const response = await fetch(url, {
+//     method: 'GET',
+//   });
+
+//   if (!response.ok) {
+//     console.error(`Call to ${url} failed with status ${response.status}`);
+//     throw new Error(`HTTP error! Status: ${response.status}`);
+//   }
+
+//   return response.json();
+// };
+
+export const calculateScore = () => {
+  return null;
+};
+
+export const getScore = () => {
+  return null;
+};
